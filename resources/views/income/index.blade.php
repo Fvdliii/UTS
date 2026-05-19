72fea46c3dd75dd87dd474449cf35d4acedfcabb
Fvdliii <fgunawan528@gmail.com>
2026-05-19 10:44:16 +0800

    Perbaruan untuk read income

--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -10,31 +10,72 @@
 
     <a class="btn btn-primary mb-3" href="{{ route('income.create') }}" role="button">Tambahkan</a>
 
-    <ul class="list-group">
-        @foreach ($incomes as $income)
-            <li class="list-group-item">
-                {{ $incomes->firstItem() + $loop->index }}.
-                {{ $income->from }} --
-                {{ $income->income }} --
-                Rp {{ number_format($income->nominal, 0, ',', '.') }} --
-                {{ $income->tanggal_income }}
+    <div class="table-responsive">
 
-                <a class="btn btn-warning btn-sm" href="{{ route('income.edit', $income) }}" role="button">Edit</a>
-                <a class="btn btn-info btn-sm" href="{{ route('income.show', $income) }}" role="button">Detail</a>
+        <table class="table table-striped table-bordered mt-3">
 
-                <form action="{{ route('income.destroy', $income) }}" method="POST" class="d-inline">
-                    @method('DELETE')
-                    @csrf
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>From</th>
+                    <th>Income</th>
+                    <th>Nominal</th>
+                    <th>Tanggal</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
 
-                    <button type="submit" class="btn btn-danger btn-sm"
-                        onclick="return confirm('Apakah anda yakin ingin menghapus data anda?')">Delete</button>
-                </form>
+            <tbody>
+                @foreach ($incomes as $income)
+                    <tr>
+                        <td>
+                            {{ $incomes->firstItem() + $loop->index }}
+                        </td>
 
+                        <td>
+                            {{ $income->from }}
+                        </td>
 
+                        <td>
+                            {{ $income->income }}
+                        </td>
 
-            </li>
-        @endforeach
-    </ul>
+                        <td>
+                            Rp {{ number_format($income->nominal, 0, ',', '.') }}
+                        </td>
+
+                        <td>
+                            {{ $income->tanggal_income }}
+                        </td>
+
+                        <td>
+                            <a class="btn btn-info btn-sm" href="{{ route('income.show', $income) }}">
+                                Detail
+                            </a>
+
+                            <a class="btn btn-warning btn-sm" href="{{ route('income.edit', $income) }}">
+                                Edit
+                            </a>
+
+                            <form action="{{ route('income.destroy', $income) }}" method="POST" class="d-inline">
+
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn btn-danger btn-sm"
+                                    onclick="return confirm('Apakah anda yakin ingin menghapus data anda?')">
+                                    Delete
+                                </button>
+
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+
+    </div>
 
     {{ $incomes->links() }}
 </x-app>
