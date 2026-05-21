a8ab3f7550de47def97f6c841fd76ada5a3347ca
Fvdliii <fgunawan528@gmail.com>
2026-05-21 16:56:39 +0800

    Revert "feat: penambahan field baru dan soft deletes pada tabel [income.recycle]"
    This reverts commit f11f5afc3c6a750215e6516a825ee76b838eaa17.

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['income', 'from', 'nominal', 'tanggal_income'])]
 
@@ -13,7 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Income extends Model
 {
     /** @use HasFactory<\Database\Factories\IncomeFactory> */
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     public function outcomes()
     {
